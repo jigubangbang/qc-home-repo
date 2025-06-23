85d8fbcb04b8629dcffa71ad851bdef51e5ce3f0
dried021 <dried021@gmail.com>
2025-06-23 09:22:31 +0900

    user quest

--- quest-service/src/main/java/com/jigubangbang/quest_service/QuestServiceApplication.java
+++ quest-service/src/main/java/com/jigubangbang/quest_service/QuestServiceApplication.java
@@ -4,8 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 @MapperScan("com.jigubangbang.quest_service.repository")
 public class QuestServiceApplication {
---
+++ quest-service/src/main/java/com/jigubangbang/quest_service/controller/UserQuestController.java
@@ -0,0 +1,14 @@
+package com.jigubangbang.quest_service.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user-quests")
+public class UserQuestController {
+    //@Autowired
+    //private UserQuestService userQuestService;
+
+    
+}
