5fb8dd2c61fec2e7a134f9ecc9aa42fa93f6ca0a
seolyeong333 <33qqpp@naver.com>
2025-07-09 12:42:28 +0900

    Succeeded

--- com-service/src/main/java/com/jigubangbang/com_service/repository/TravelmateMapper.java
+++ com-service/src/main/java/com/jigubangbang/com_service/repository/TravelmateMapper.java
@@ -12,9 +12,6 @@ import com.jigubangbang.com_service.model.CountryDto;
 @Mapper
 public interface TravelmateMapper {
     //검색
-    // @Select("SELECT id, name, continent FROM country ORDER BY name ASC")
     List<CountryDto> selectAllCountries();
-
-    // @Select("SELECT id, city_name, country_id FROM city WHERE country_id = #{countryId} ORDER BY city_name ASC")
     List<CityDto> selectCitiesByCountryId(String countryId);
 }
